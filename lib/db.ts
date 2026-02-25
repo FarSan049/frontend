dc11cb9ae4bd79e8351740bc7482f4c4e0d016b5
FarSan049 <Farizikhsan049@gmail.com>
2026-02-26 06:36:30 +0700

    change env and connected

--- lib/db.ts
+++ lib/db.ts
@@ -7,11 +7,16 @@ const globalForPrisma = global as unknown as {
 };
 
 // initialize prisma client
+const url = (process.env.PRISMA_DATABASE_URL || process.env.DATABASE_URL) as string;
+const isAccelerate = url?.startsWith("prisma://") || url?.startsWith("prisma+postgres://");
+
+const prismaOptions: any = isAccelerate
+  ? { accelerateUrl: url }
+  : { datasourceUrl: url };
+
 const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL as string,
-  });
+  new PrismaClient(prismaOptions);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
