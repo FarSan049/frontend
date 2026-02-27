4d65dc03f640d6438373acfe7452b1f7a084d670
FarSan049 <Farizikhsan049@gmail.com>
2026-02-28 06:57:06 +0700

    chore: update package.json to include prisma and modify prisma schema generator

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 // ensure only a single instance of prisma client is created
